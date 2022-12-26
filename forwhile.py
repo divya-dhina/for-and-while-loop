825c19e123ec6d1e6e1cfee9544f42e568d7bafb
divya-dhina <121435130+divya-dhina@users.noreply.github.com>
2022-12-26 18:14:49 +0530

    Create forwhile.py

---
+++ forwhile.py
@@ -0,0 +1,11 @@
+#python 3.7.1
+
+for i in range(1,10):
+ print (i)
+for a in range(1,10,2):
+  print(a)
+a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+while (a<b):
+  print(a)
+  a=a+1
